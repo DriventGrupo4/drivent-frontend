24ddf887a171247a3ac3595059dff18379c525d7
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-03-02 20:02:12 -0300

    feat: warining no enrollment

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,15 +1,53 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import useEnrollment from '../../../hooks/api/useEnrollment';
 
 export default function Payment() {
+  const [clickedPresencial, setClickedP] = useState(false);
+  const [clickedRemote, setClickedR] = useState(false);
+  
+  const { enrollment } = useEnrollment();
+  if (!enrollment) {
+    return (
+      <>
+        <Title>Ingressos e pagamentos</Title>
+        <Warning>
+          <h5>Você precisa completar sua inscrição antes
+            de prosseguir pra escolha de ingresso</h5>
+        </Warning>
+      </>
+    );
+  }
+
+  function Ispresincial() {
+    if (clickedRemote === true) {
+      setClickedR(false);
+    }
+    if (clickedPresencial === false) {
+      return setClickedP(true);
+    }
+    setClickedP(false);
+  }
+
+  function Isremote() {
+    if (clickedPresencial === true) {
+      setClickedP(false);
+    }
+    if (clickedRemote === false) {
+      return setClickedR(true);
+    }
+    setClickedR(false);
+  }
+
   return <>
     <Title>Ingressos e pagamentos</Title>
     <Subtile>Primeiro, escolha sua modalidade de ingresso</Subtile>
     <TicketContainer>
-      <Presencial>
+      <Presencial onClick={Ispresincial} clickedPresencial={clickedPresencial}>
         <h3>Presencial</h3>
         <h4>R$250</h4>
       </Presencial>
-      <Remote>
+      <Remote onClick={Isremote} clickedRemote={clickedRemote}>
         <h3>Online</h3>
         <h4>R$100</h4>
       </Remote>
@@ -57,9 +95,10 @@ width: 145px;
 height: 145px;
 left: 341px;
 margin-top: 17px;
-
+background-color: ${(prop) => prop.clickedPresencial ? '#FFEED2' : 'transparent'};
 border: 1px solid #CECECE;
 border-radius: 20px;
+cursor: pointer;
 h3{
   width: 75px;
 height: 19px;
@@ -105,9 +144,10 @@ height: 145px;
 left: 341px;
 margin-top: 17px;
 margin-left: 24px;
-
+background-color: ${(prop) => prop.clickedRemote ? '#FFEED2' : 'transparent'};
 border: 1px solid #CECECE;
 border-radius: 20px;
+cursor: pointer;
 h3{
   width: 75px;
 height: 19px;
@@ -145,4 +185,19 @@ color: #898989;
 const TicketContainer = styled.div`
   display: flex;
 
+`;
+
+const Warning = styled.div`
+   width: 100%;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h5{
+    text-align: center;
+    width: 30rem;
+    height: 2.5rem;
+    font-size: 20px;
+    color: #8E8E8E;
+  }
 `;
