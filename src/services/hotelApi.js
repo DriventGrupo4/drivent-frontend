69a87ae2d739900b7b9239c6628c62e175eefc71
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-03-10 17:40:35 -0300

    feat: hotel with api

---
+++ src/services/hotelApi.js
@@ -0,0 +1,19 @@
+import api from './api';
+
+export async function getHotels(token) {
+  const response = await api.get('/hotels', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export async function getHotelsById(token, hotelId) {
+  const response = await api.get(`/hotels/${hotelId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
