164929929f6eed2c1a50a5db7bc53ee233eac8b1
Ahranna Araujo <ahrannaaraujo@Ahrannas-MacBook-Air.local>
2023-03-15 14:43:39 -0300

    activities

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,25 @@
+import styled from 'styled-components';
+import ChooseActivitie from '../../../components/ActivitiesInformation/ChooseActivities';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  return (
+    <>
+      <Title>Escolha de atividades</Title>
+      <ChooseActivitie></ChooseActivitie>
+    </>
+  );
 }
+
+const Title = styled.h1 `
+width: 600px;
+height: 40px;
+left: 341px;
+top: 206px;
+
+font-weight: 400;
+font-size: 34px;
+line-height: 40px;
+
+color: #000000;
+
+`;
