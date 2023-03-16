782a39ec579008fea22234cf48dcd8f9efc4cd99
Andreia Vieira <andreiavieira2353@gmail.com>
2023-03-15 21:27:13 -0300

    Activities Validation

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -10,7 +10,7 @@ import { getBooking } from '../../../services/bookingAPI';
 export default function Hotel() {
   const { userData } = useContext(UserContext);
   const [payment, setPayment] = useState();
-  const [ bookingId, setBookingId ] = useState('');
+  const [bookingId, setBookingId] = useState('');
   const [display, setDisplay] = useState('');
   const [display2, setDisplay2] = useState('');
 
@@ -26,55 +26,72 @@ export default function Hotel() {
 
   return (
     <>
-      {payment?.status === 'PAID' && payment?.ticketTypeId === 1 ?(
+      {payment?.status === 'PAID' && payment?.ticketTypeId === 1 ? (
         <>
           <Title>Escolha de hotel e quarto</Title>
-          { display === '' && display2 === 'none' ?
-            <HotelPayment setBookingId={setBookingId} setDisplay={setDisplay} display={display} 
-              setDisplay2={setDisplay2} /> : ''
-          }
-          
+          {bookingId === '' ? (
+            <HotelPayment
+              setBookingId={setBookingId}
+              setDisplay={setDisplay}
+              display={display}
+              setDisplay2={setDisplay2}
+            />
+          ) : display === '' && display2 === 'none' ? (
+            <HotelPayment
+              setBookingId={setBookingId}
+              setDisplay={setDisplay}
+              display={display}
+              setDisplay2={setDisplay2}
+            />
+          ) : (
+            ''
+          )}
         </>
       ) : (
         <>
           <Warning>
             {payment !== undefined && payment?.ticketTypeId !== 1 ? (
-              <h5>Sua modalidade de ingresso não inclui hospedagem
-              Prossiga para a escolha de atividades</h5>
+              <h5>Sua modalidade de ingresso não inclui hospedagem Prossiga para a escolha de atividades</h5>
             ) : (
-              <h5>Você precisa ter confirmado pagamento antes
-              de fazer a escolha de hospedagem</h5>
+              <h5>Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem</h5>
             )}
           </Warning>
         </>
       )}
-      {bookingId? <PersonalHotelInformation setDisplay={setDisplay} display={display}
-        setDisplay2={setDisplay2} display2={display2} /> : ''}
-      
+      {bookingId ? (
+        <PersonalHotelInformation
+          setDisplay={setDisplay}
+          display={display}
+          setDisplay2={setDisplay2}
+          display2={display2}
+        />
+      ) : (
+        ''
+      )}
     </>
   );
 }
 const Title = styled.h1`
-      width: 600px;
-      height: 40px;
-      left: 321px;
-      top: 206px;
-      font-weight: 400;
-      font-size: 34px;
-      line-height: 40px;
-      color: #000000;
-      `;
+  width: 600px;
+  height: 40px;
+  left: 321px;
+  top: 206px;
+  font-weight: 400;
+  font-size: 34px;
+  line-height: 40px;
+  color: #000000;
+`;
 const Warning = styled.div`
-      width: 100%;
-      height: 70vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      h5 {
-        text-align: center;
-      width: 30rem;
-      height: 2.5rem;
-      font-size: 20px;
-      color: #8e8e8e;
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h5 {
+    text-align: center;
+    width: 30rem;
+    height: 2.5rem;
+    font-size: 20px;
+    color: #8e8e8e;
   }
-      `;
+`;
