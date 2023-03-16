97e4fcc6a3190aca637aa83c437b86701b301a2d
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-03-16 18:17:44 -0300

    feat: activities no hour

--- src/components/ActivitiesInformation/ChooseActivities.js
+++ src/components/ActivitiesInformation/ChooseActivities.js
@@ -1,30 +1,83 @@
+import { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import UserContext from '../../contexts/UserContext';
+import { findActivities } from '../../services/activitiesApi';
+import { LateralActivities } from './LateralActivities';
+import { PrincipalActivities } from './PrincipalActivities';
+import { WorkShopeRoom } from './WorkShopeRoom';
 
 export default function ChooseActivitie() {
+  const { userData } = useContext(UserContext);
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await findActivities(userData.token);
+      setActivities(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <Container>
-      <Information>
-        <h2>Minecraft: montando o PC ideal</h2>
-        <h3>09:00 - 10:00</h3>
+      <AuditorioPrincipal>
+        <h1>Auditório Principal</h1>
+        <AuditorioSeparation>{activities.map((a, i) => <PrincipalActivities a = {a} key = {a.id} i = {i}/>)}</AuditorioSeparation>
+      </AuditorioPrincipal>
+      <AuditorioLateral>
+        <h1>Auditório Lateral</h1>
+        <AuditorioSeparation>{activities.map((a) => <LateralActivities a = {a} key = {a.id} />)}</AuditorioSeparation>
+      </AuditorioLateral>
+      <WorkShope>
+        <h1>Sala de WorkShope</h1>
+        <AuditorioSeparation>{activities.map((a) => <WorkShopeRoom a = {a} key = {a.id}/>)}</AuditorioSeparation>
+      </WorkShope>
+      {/* <Information>
+        {activities.map((a) => <Activities a = {a} key = {a.id}/>)}
       </Information>
       <Line></Line>
       <Availability>
         <h2><ion-icon name="enter-outline"></ion-icon></h2>
         <h3>27 vagas</h3>
-      </Availability>
+      </Availability> */}
     </Container>
   );
-}
+};
 
-const Container = styled.div `
-width: 265px;
-height: 79px;
-left: 350px;
-margin-top: 125px;
-background: #F1F1F1;
-border-radius: 5px;
-display: flex;
+const AuditorioPrincipal = styled.div`
 `;
+
+const AuditorioSeparation = styled.div`
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  width: 300px;
+  height: 450px;  
+  border: 1px solid #CFCFCF;
+`;
+
+const AuditorioLateral = styled.div`
+`;
+
+const WorkShope = styled.div`
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 50px;
+  h1{
+    margin-top: 70px;
+    display: flex;
+    margin-bottom:13px;
+    justify-content: center;
+font-style: normal;
+font-weight: 400;
+font-size: 17px;
+color: #7B7B7B;
+  }
+`;
+
 const Information = styled.div `
 h2 {
   margin-top: 10px;
@@ -45,28 +98,5 @@ h2 {
  font-size: 12px;
  line-height: 14px;
  color: #343434;
-`;
-const Line = styled.div `
-margin-top: 10px;
-margin-left: 15px;
-height: 60px;
-border-left: 1px solid #CFCFCF
-`;
-const Availability = styled.div `
-h2 {
-  margin-top: 15px;
-  margin-left: 20px;
-  font-weight: 900;
-  font-size: 25px;
-  color: #078632;
-}
- h3 {
- margin-left: 16px;
- font-family: 'Roboto';
- font-style: normal;
- font-weight: 400;
- font-size: 11px;
- line-height: 11px;
- color: #078632;
- }
-`;
+ }`;
+
